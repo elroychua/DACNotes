7e1ac92ea20fd55df7c596d07bf2bdc67337a5a1
Elroy Chua <elroymx@gmail.com>
2023-01-12 00:50:38 +0800

    lesson 4 notes

--- README.md
+++ README.md
@@ -101,3 +101,34 @@ finally:
     driver.quit()
 
 ```
+
+### Tutorial 4
+
+#### Action chain
+
+```Python
+driver.get("https://orteil.dashnet.org/cookieclicker/")
+# wont pass this line until 5 sec is up.
+driver.implicitly_wait(5)
+language = driver.find_element(By.ID, "langSelect-EN")
+cookie = driver.find_element(By.ID, "bigCookie")
+cookie_count = driver.find_element(By.ID, "cookies")
+items = [driver.find_element(By.ID, "productPrice"+str(i))
+         for i in range(1, -1, -1)]  # start at most expensive till last.
+
+# Sets up chain of actions to run
+selectLanguageAction = ActionChains(driver)
+selectLanguageAction.click(language)
+selectLanguageAction.perform()
+
+driver.implicitly_wait(20)
+
+actions = ActionChains(driver)
+actions.click(cookie)
+
+for i in range(5000):
+    actions.perform()
+    count = cookie_count.text
+    print(count)
+
+```
--- Tutorial1.py
+++ Tutorial1.py
@@ -1,36 +1,31 @@
 # Selenium Tutorial 1
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 import time
-
 PATH = '/user/local/bin/chromedriver'
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://techwithtim.net")
-# select python programming
-link = driver.find_element(By.LINK_TEXT, "Python Programming")
-link.click()
+driver.get("https://orteil.dashnet.org/cookieclicker/")
+# wont pass this line until 5 sec is up.
+driver.implicitly_wait(5)
+language = driver.find_element(By.ID, "langSelect-EN")
+cookie = driver.find_element(By.ID, "bigCookie")
+cookie_count = driver.find_element(By.ID, "cookies")
+items = [driver.find_element(By.ID, "productPrice"+str(i))
+         for i in range(1, -1, -1)]  # start at most expensive till last.
 
-try:
-    # Click on beginner python tutorials
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(
-            (By.LINK_TEXT, "Beginner Python Tutorials"))
-    )
+# Sets up chain of actions to run
+selectLanguageAction = ActionChains(driver)
+selectLanguageAction.click(language)
+selectLanguageAction.perform()
 
-    element.click()
-    # Accesses a href
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(
-            (By.ID, "sow-button-19310003"))
-    )
-    element.click()
-    # Goes back
-    driver.back()
-    # Goes forward
-    driver.forward()
-finally:
-    driver.quit()
+driver.implicitly_wait(20)
+
+actions = ActionChains(driver)
+actions.click(cookie)
+
+for i in range(5000):
+    actions.perform()
+    count = cookie_count.text
+    print(count)
